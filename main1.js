5fddf491ade154524acd4de2b39527b0f1caac8c
harim <p66389416@gmail.com>
2024-11-08 16:07:37 +0900

    index1.html

---
+++ main1.js
@@ -0,0 +1,17 @@
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const message = document.getElementById("message");
+
+  // 가상의 유효한 사용자 정보 (예: 아이디 "naveruser", 비밀번호 "1234")
+  const validUsername = "name";
+  const validPassword = "1234";
+
+  if (username === validUsername && password === validPassword) {
+    message.style.color = "green";
+    message.textContent = "로그인 성공!";
+  } else {
+    message.style.color = "red";
+    message.textContent = "아이디 또는 비밀번호가 잘못 되었습니다.";
+  }
+}
